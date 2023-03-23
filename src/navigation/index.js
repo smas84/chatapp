73d6fde75c73f431584b72957785eed2f00f93cc
Hamed Zadeh <smasalehizadeh@gmail.com>
2023-03-23 15:28:43 -0700

    UI-8 Contact List

---
+++ src/navigation/index.js
@@ -0,0 +1,24 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import MainTabNavigator from "./MainTabNavigator";
+import ChatListScreen from "../screens/ChatListScreen";
+import ChatScreen from "../screens/ChatScreen";
+import ContactsScreen from "../screens/ContactsScreen";
+
+const Stack = createNativeStackNavigator();
+
+const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerStyle: { backgroundColor: "whitesmoke" }}}>
+        <Stack.Screen name="MainTab" component={MainTabNavigator} options={{ headerShown: false }}/>      
+        <Stack.Screen name="My Chats" component={ChatListScreen} />
+        <Stack.Screen name="Chat Content" component={ChatScreen} />
+        <Stack.Screen name="Contacts" component={ContactsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigator;
