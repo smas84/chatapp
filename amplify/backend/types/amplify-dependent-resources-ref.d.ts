9949584eab7023239a977f035ed550102cc8f359
Hamed Zadeh <smasalehizadeh@gmail.com>
2023-03-25 12:28:08 -0700

    BE-2 Data Model and Users

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "Yaca": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "Yaca": {
       "AppClientID": "string",
